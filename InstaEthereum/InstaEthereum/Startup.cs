87abc56a19cea1e2d770d9aa1c53f45ed24d8b68
vijay babar <vijbab.kalp@gmail.com>
2020-07-29 23:34:44 +0530

    InstaEthereum Asp.Net MVC 5 Project Created.

---
+++ InstaEthereum/InstaEthereum/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(InstaEthereum.Startup))]
+namespace InstaEthereum
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
