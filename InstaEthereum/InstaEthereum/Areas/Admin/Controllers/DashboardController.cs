319483b5be124ee8f64e77026e513c7bb288d8c9
vijay babar <vijbab.kalp@gmail.com>
2020-07-30 13:55:16 +0530

    Set Authorize Role

--- InstaEthereum/InstaEthereum/Areas/Admin/Controllers/DashboardController.cs
+++ InstaEthereum/InstaEthereum/Areas/Admin/Controllers/DashboardController.cs
@@ -1,17 +1,41 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Web;
 using System.Web.Mvc;
+using InstaEthereum.Models;
+using Microsoft.AspNet.Identity;
+using Microsoft.Owin.Security;
 
 namespace InstaEthereum.Areas.Admin.Controllers
 {
+    [Authorize(Roles = RoleName.Admin )]
     public class DashboardController : Controller
-    {
-        // GET: Admin/Dashboard
+    {        
         public ActionResult Index()
         {
             return View();
+        }
+
+        // Admin Logout
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult LogOff()
+        {
+            AuthenticationManager.SignOut(DefaultAuthenticationTypes.ApplicationCookie);
+
+            var baseUrl = Request.Url.Scheme + "://" + Request.Url.Authority;
+            var url = baseUrl + "/Account/Login";
+            return Redirect(url);
+        }
+
+        private IAuthenticationManager AuthenticationManager
+        {
+            get
+            {
+                return HttpContext.GetOwinContext().Authentication;
+            }
         }
     }
 }
---
+++ InstaEthereum/InstaEthereum/Models/RoleName.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace InstaEthereum.Models
+{
+    public class RoleName
+    {
+        public const string Admin = "Admin";
+        public const string User = "User";
+    }
+}
