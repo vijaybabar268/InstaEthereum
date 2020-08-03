0ef09471f2b8bebefb6e63a87ff22bf894188014
vijay babar <vijbab.kalp@gmail.com>
2020-08-03 17:58:21 +0530

    MS SQL Server Database Setup.

--- InstaEthereum/InstaEthereum/Models/IdentityModels.cs
+++ InstaEthereum/InstaEthereum/Models/IdentityModels.cs
@@ -8,7 +8,6 @@ using System.Threading.Tasks;
 using System.Web.Security;
 using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.EntityFramework;
-using MySql.Data.Entity;
 
 namespace InstaEthereum.Models
 {
@@ -27,65 +26,20 @@ namespace InstaEthereum.Models
             return userIdentity;
         }
     }
-
-    [DbConfigurationType(typeof(MySqlEFConfiguration))]
+        
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
         public ApplicationDbContext()
-            : base("MySql_CS", throwIfV1Schema: false)
-        {
-            this.Configuration.ValidateOnSaveEnabled = false;
+            : base("DefaultConnection", throwIfV1Schema: false)
+        {            
         }
 
         public static ApplicationDbContext Create()
-        {
-            DbConfiguration.SetConfiguration(new MySqlEFConfiguration());
+        {            
             return new ApplicationDbContext();
         }
 
-        protected override void OnModelCreating(DbModelBuilder modelBuilder)
-        {
-            modelBuilder.Conventions.Remove<System.Data.Entity.ModelConfiguration.Conventions.PluralizingTableNameConvention>();
-            base.OnModelCreating(modelBuilder);
 
-            modelBuilder.Entity<ApplicationUser>().ToTable("aspnetroles");
-            //modelBuilder.Entity<ApplicationUser>().ToTable("aspnetuserroles");
-            //modelBuilder.Entity<ApplicationUser>().ToTable("aspnetusers");
-            //modelBuilder.Entity<ApplicationUser>().ToTable("aspnetuserclaims");
-            //modelBuilder.Entity<ApplicationUser>().ToTable("aspnetuserlogins");
-
-            modelBuilder.Entity<ApplicationUser>().Property(t => t.Id).HasColumnName("id");
-            //modelBuilder.Entity<ApplicationUser>().Property(t => t.Email).HasColumnName("email");
-            //modelBuilder.Entity<ApplicationUser>().Property(t => t.EmailConfirmed).HasColumnName("emailconfirmed");
-            //modelBuilder.Entity<ApplicationUser>().Property(t => t.PasswordHash).HasColumnName("passwordhash");
-            //modelBuilder.Entity<ApplicationUser>().Property(t => t.SecurityStamp).HasColumnName("securitystamp");
-            //modelBuilder.Entity<ApplicationUser>().Property(t => t.PhoneNumber).HasColumnName("phonenumber");
-            //modelBuilder.Entity<ApplicationUser>().Property(t => t.PhoneNumberConfirmed).HasColumnName("phonenumberconfirmed");
-            //modelBuilder.Entity<ApplicationUser>().Property(t => t.TwoFactorEnabled).HasColumnName("twofactorenabled");
-            //modelBuilder.Entity<ApplicationUser>().Property(t => t.LockoutEndDateUtc).HasColumnName("lockoutenddateutc");
-            //modelBuilder.Entity<ApplicationUser>().Property(t => t.LockoutEnabled).HasColumnName("lockoutenabled");
-            //modelBuilder.Entity<ApplicationUser>().Property(t => t.AccessFailedCount).HasColumnName("accessfailedcount");
-            //modelBuilder.Entity<ApplicationUser>().Property(t => t.UserName).HasColumnName("username");
-            modelBuilder.Entity<ApplicationUser>().Property(t => t.WalletAddress).HasColumnName("WalletAddress");
-            modelBuilder.Entity<ApplicationUser>().Property(t => t.WalletAddress).HasColumnName("Extent1.walletaddress");
-
-            //modelBuilder.Properties().Where(x =>
-            //        x.PropertyType.FullName != null &&
-            //        (x.PropertyType.FullName.Equals("System.String") &&
-            //        !x.GetCustomAttributes(false).OfType<ColumnAttribute>().Any(q => q.TypeName != null &&
-            //        q.TypeName.Equals("varchar(max)", StringComparison.InvariantCultureIgnoreCase)))).Configure(c =>
-            //        c.HasColumnType("varchar(65000)"));
-
-            //modelBuilder.Properties().Where(x =>
-            //        x.PropertyType.FullName != null &&
-            //        (x.PropertyType.FullName.Equals("System.String") &&
-            //        !x.GetCustomAttributes(false).OfType<ColumnAttribute>().Any(q => q.TypeName != null &&
-            //        q.TypeName.Equals("nvarchar", StringComparison.InvariantCultureIgnoreCase)))).Configure(c =>
-            //        c.HasColumnType("varchar"));
-        }
-
-        
+                
     }
-
-
 }
